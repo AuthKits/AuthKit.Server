ccc177b146f3d2d226b9b4cb35704711f6a6b22f
Rian.be <wintez@protonmail.com>
2026-09-17 23:07:57 +0200

    feat: set up Svelte 5 and Tailwind css for devtools

--- src/Plugins/Solutions/DevTools/UI/vite.config.ts
+++ src/Plugins/Solutions/DevTools/UI/vite.config.ts
@@ -1,29 +1,17 @@
-import { rename } from "node:fs/promises";
-import { join, resolve } from "node:path";
-import { defineConfig, type Plugin } from "vite";
+import { join } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
-function outputAsUiHtml(): Plugin {
-  return {
-    name: "ui-html-output-name",
-    apply: "build",
-    async writeBundle() {
-      const out = resolve(import.meta.dirname, "dist");
-      await rename(join(out, "template.html"), join(out, "ui.html"));
-    },
-  };
-}
-
 export default defineConfig({
-  root: resolve(import.meta.dirname),
+  root: import.meta.dirname,
   build: {
     outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        ui: resolve(import.meta.dirname, "template.html"),
-      },
+      input: join(import.meta.dirname, "template.html"),
     },
   },
-  plugins: [viteSingleFile(), outputAsUiHtml()],
+  plugins: [svelte(), tailwindcss(), viteSingleFile()],
 });
